e5065b6b77725fda5e59c9ee920a4a1f9cf41e85
Yans-droid <riantoagustinu2000@gmail.com>
2024-11-13 23:05:12 +0700

    update

---
+++ app/src/main/java/com/rian/jambonnotes/splash_activity.kt
@@ -0,0 +1,18 @@
+package com.rian.jambonnotes
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class splash_activity :AppCompatActivity (){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)//Durasi 3 detik
+    }
+}
